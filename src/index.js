570ad427a6d4871e83cc26ca8117e5ae4ee69ec8
Kevin London <kevinlondon@gmail.com>
2017-09-02 13:57:01 -0600

    A basic commit with the todo app

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import todoApp from './reducers'
+import App from './components/App'
+
+let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
